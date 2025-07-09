72ae02aeb8a84fa1a3514a47230dac0ee9c1f5fc
soumyasw2014 <soumya.sw2014@gmail.com>
2025-07-09 10:26:09 +0530

    Soumya- added String example two

---
+++ src/main/java/com/example/reactivecruddemo/others/StringExample2.java
@@ -0,0 +1,7 @@
+package com.example.reactivecruddemo.others;
+
+public class StringExample2 {
+    public static void main(String[] args) {
+        System.out.println("this is second string example");
+    }
+}
